01a83fe5e6934824e11d8c654aa19edc4e52590e
Anton Yabchinskiy <arn@bestmx.ru>
2015-02-17 13:09:23 +0300

    Add List_ext.Non_empty.iter function

--- list_ext.ml
+++ list_ext.ml
@@ -4,6 +4,8 @@ module Non_empty =
   struct
     type 'a t = 'a * 'a list
 
+    let iter f (h, t) = f h; iter f t
+
     let map f (h, t) = f h, map f t
 
     let of_list = function
--- list_ext.mli
+++ list_ext.mli
@@ -4,6 +4,8 @@ module Non_empty :
 sig
   type 'a t = 'a * 'a list
 
+  val iter : ('a -> unit) -> 'a t -> unit
+
   val map : ('a -> 'b) -> 'a t -> 'b t
 
   val of_list : 'a list -> 'a t
